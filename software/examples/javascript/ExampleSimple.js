7ea382a6d5183210d8449128bf3b9a82a126d99b
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-03-10 12:46:09 +0100

    javascript: adding color bricklet examples

---
+++ software/examples/javascript/ExampleSimple.js
@@ -0,0 +1,42 @@
+var Tinkerforge = require('tinkerforge');
+
+var HOST = 'localhost';
+var PORT = 4223;
+var UID = 'abc';// Change to your UID
+
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var c = new Tinkerforge.BrickletColor(UID, ipcon);// Create device object
+
+ipcon.connect(HOST, PORT,
+    function(error) {
+        console.log('Error: '+error);        
+    }
+);// Connect to brickd
+
+// Don't use device before ipcon is connected
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
+    function(connectReason) {
+        // Get current color
+        c.getColor(
+            function(r, g, b, C) {
+                console.log('Color(R): '+r);
+                console.log('Color(G): '+g);
+                console.log('Color(B): '+b);
+                console.log('Color(C): '+C);
+                console.log();
+            },
+            function(error) {
+                console.log('Error: '+error);
+            }
+        );
+    }
+);
+
+console.log("Press any key to exit ...");
+process.stdin.on('data',
+    function(data) {
+        ipcon.disconnect();
+        process.exit(0);
+    }
+);
+
