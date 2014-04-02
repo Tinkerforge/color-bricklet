c9335ab382a0f3d06fc598e05916b4865365ae27
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 17:21:30 +0200

    java: Unify constants style

--- software/examples/java/ExampleCallback.java
+++ software/examples/java/ExampleCallback.java
@@ -2,36 +2,36 @@ import com.tinkerforge.BrickletColor;
 import com.tinkerforge.IPConnection;
 
 public class ExampleCallback {
-    private static final String host = "localhost";
-    private static final int port = 4223;
-    private static final String UID = "abc"; // Change to your UID
-    
-    // Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-    //       might normally want to catch are described in the documentation
-    public static void main(String args[]) throws Exception {
-        IPConnection ipcon = new IPConnection(); // Create IP connection
-        BrickletColor c = new BrickletColor(UID, ipcon); // Create device object
+	private static final String HOST = "localhost";
+	private static final int PORT = 4223;
+	private static final String UID = "abc"; // Change to your UID
 
-        ipcon.connect(host, port); // Connect to brickd
-        // Don't use device before ipcon is connected
+	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+	//       might normally want to catch are described in the documentation
+	public static void main(String args[]) throws Exception {
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletColor c = new BrickletColor(UID, ipcon); // Create device object
 
-        // Set Period for color callback to 1s (1000ms)
-        // Note: The color callback is only called every second if the 
-        //       color has changed since the last call!
-        c.setColorCallbackPeriod(1000);
+		ipcon.connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
 
-        // Add and implement color listener (called if color changes)
-        c.addColorListener(new BrickletColor.ColorListener() {
-            public void color(int r, int g, int b, int c) {
-                System.out.println("Color(R): " + r);
-                System.out.println("Color(G): " + g);
-                System.out.println("Color(B): " + b);
-                System.out.println("Color(C): " + c);
-                System.out.println("");
-            }
-        });
+		// Set Period for color callback to 1s (1000ms)
+		// Note: The color callback is only called every second if the
+		//       color has changed since the last call!
+		c.setColorCallbackPeriod(1000);
 
-        System.console().readLine("Press key to exit\n");
-        ipcon.disconnect();
-    }
+		// Add and implement color listener (called if color changes)
+		c.addColorListener(new BrickletColor.ColorListener() {
+			public void color(int r, int g, int b, int c) {
+				System.out.println("Color(R): " + r);
+				System.out.println("Color(G): " + g);
+				System.out.println("Color(B): " + b);
+				System.out.println("Color(C): " + c);
+				System.out.println("");
+			}
+		});
+
+		System.console().readLine("Press key to exit\n");
+		ipcon.disconnect();
+	}
 }
--- software/examples/java/ExampleSimple.java
+++ software/examples/java/ExampleSimple.java
@@ -3,29 +3,29 @@ import com.tinkerforge.BrickletColor.Color;
 import com.tinkerforge.IPConnection;
 
 public class ExampleSimple {
-    private static final String host = "localhost";
-    private static final int port = 4223;
-    private static final String UID = "abc"; // Change to your UID
-    
-    // Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-    //       might normally want to catch are described in the documentation
-    public static void main(String args[]) throws Exception {
-        IPConnection ipcon = new IPConnection(); // Create IP connection
-        BrickletColor c = new BrickletColor(UID, ipcon); // Create device object
+	private static final String HOST = "localhost";
+	private static final int PORT = 4223;
+	private static final String UID = "abc"; // Change to your UID
 
-        ipcon.connect(host, port); // Connect to brickd
-        // Don't use device before ipcon is connected
+	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+	//       might normally want to catch are described in the documentation
+	public static void main(String args[]) throws Exception {
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickletColor c = new BrickletColor(UID, ipcon); // Create device object
 
-        // Get current color
-        Color color = c.getColor(); // Can throw com.tinkerforge.TimeoutException
+		ipcon.connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
 
-        System.out.println("Color(R): " + color.r);
-        System.out.println("Color(G): " + color.g);
-        System.out.println("Color(B): " + color.b);
-        System.out.println("Color(C): " + color.c);
-        System.out.println("");
-        
-        System.console().readLine("Press key to exit\n");
-        ipcon.disconnect();
-    }
+		// Get current color
+		Color color = c.getColor(); // Can throw com.tinkerforge.TimeoutException
+
+		System.out.println("Color(R): " + color.r);
+		System.out.println("Color(G): " + color.g);
+		System.out.println("Color(B): " + color.b);
+		System.out.println("Color(C): " + color.c);
+		System.out.println("");
+
+		System.console().readLine("Press key to exit\n");
+		ipcon.disconnect();
+	}
 }
