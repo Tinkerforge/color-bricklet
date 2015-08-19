de68479ec95555d37fe8643c53daea6b61335c7c
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 15:40:40 +0200

    Cleanup Perl examples

--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -5,7 +5,7 @@ use Tinkerforge::BrickletColor;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'abc'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $c = Tinkerforge::BrickletColor->new(&UID, $ipcon); # Create device object
